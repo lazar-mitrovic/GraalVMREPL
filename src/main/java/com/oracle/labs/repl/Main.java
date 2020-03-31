0de5c622f4b1873097f8ed299df6262a9185f008
jose.pereda <jose.pereda@gluonhq.com>
2020-03-31 19:57:33 +0200

    Replace SplitPane with VBox Make caret node unmanaged for better control of its position

--- src/main/java/com/oracle/labs/repl/Main.java
+++ src/main/java/com/oracle/labs/repl/Main.java
@@ -1,10 +1,13 @@
 package com.oracle.labs.repl;
 
+import com.gluonhq.attach.display.DisplayService;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Dimension2D;
 import javafx.scene.Scene;
 import javafx.scene.Parent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,7 +17,8 @@ public class Main extends Application {
     @Override
     public void start(final Stage stage) throws IOException {
         final Parent root = FXMLLoader.load(Main.class.getResource("/main.fxml"));
-        final Scene scene = new Scene(root);
+        Dimension2D dimension2D = DisplayService.create().map(d -> d.getDefaultDimensions()).orElse(new Dimension2D(667, 350));
+        final Scene scene = new Scene(root, dimension2D.getWidth(), dimension2D.getHeight(), Color.web("#03687f"));
         stage.setScene(scene);
         stage.setTitle("GraalVM REPL");
         stage.show();
