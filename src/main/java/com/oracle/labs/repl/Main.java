8016b9c418ceb816bebfce2cb8347911508e6a3c
Lazar Mitrović <lazar.mitrovic@msn.com>
2021-05-18 03:40:50 +0200

    Add License

--- src/main/java/com/oracle/labs/repl/Main.java
+++ src/main/java/com/oracle/labs/repl/Main.java
@@ -1,3 +1,43 @@
+/*
+ * Copyright (c) 2020, 2021 Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * The Universal Permissive License (UPL), Version 1.0
+ *
+ * Subject to the condition set forth below, permission is hereby granted to any
+ * person obtaining a copy of this software, associated documentation and/or
+ * data (collectively the "Software"), free of charge and under any and all
+ * copyright rights in the Software, and any and all patent rights owned or
+ * freely licensable by each licensor hereunder covering either (i) the
+ * unmodified Software as contributed to or provided by such licensor, or (ii)
+ * the Larger Works (as defined below), to deal in both
+ *
+ * (a) the Software, and
+ *
+ * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+ * one is included with the Software each a "Larger Work" to which the Software
+ * is contributed by such licensors),
+ *
+ * without restriction, including without limitation the rights to copy, create
+ * derivative works of, display, perform, and distribute the Software and make,
+ * use, sell, offer for sale, import, export, have made, and have sold the
+ * Software and the Larger Work(s), and to sublicense the foregoing rights on
+ * either these or other terms.
+ *
+ * This license is subject to the following condition:
+ *
+ * The above copyright notice and either this complete permission notice or at a
+ * minimum a reference to the UPL must be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.oracle.labs.repl;
 
 import com.gluonhq.attach.display.DisplayService;
@@ -5,20 +45,26 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Dimension2D;
-import javafx.scene.Scene;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
 
     @Override
     public void start(final Stage stage) throws IOException {
-        final Parent root = FXMLLoader.load(Main.class.getResource("/main.fxml"));
-        Dimension2D dimension2D = DisplayService.create().map(d -> d.getDefaultDimensions()).orElse(new Dimension2D(667, 350));
+        Font.loadFont(Objects.requireNonNull(Main.class.getResource("/Inconsolata-Regular.ttf")).toExternalForm(), 10);
+        Font.loadFont(Objects.requireNonNull(Main.class.getResource("/FreeMono.ttf")).toExternalForm(), 10);
+
+        final Parent root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("/main.fxml")));
+        Dimension2D dimension2D = DisplayService.create().map(DisplayService::getDefaultDimensions).orElse(new Dimension2D(667, 350));
         final Scene scene = new Scene(root, dimension2D.getWidth(), dimension2D.getHeight(), Color.web("#03687f"));
+        // scene.getStylesheets().add("/styles.css");
         stage.setScene(scene);
         stage.setTitle("GraalVM REPL");
         stage.show();
--- src/main/java/com/oracle/labs/repl/util/ZipUtils.java
+++ src/main/java/com/oracle/labs/repl/util/ZipUtils.java
@@ -1,6 +1,50 @@
+/*
+ * Copyright (c) 2020, 2021 Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * The Universal Permissive License (UPL), Version 1.0
+ *
+ * Subject to the condition set forth below, permission is hereby granted to any
+ * person obtaining a copy of this software, associated documentation and/or
+ * data (collectively the "Software"), free of charge and under any and all
+ * copyright rights in the Software, and any and all patent rights owned or
+ * freely licensable by each licensor hereunder covering either (i) the
+ * unmodified Software as contributed to or provided by such licensor, or (ii)
+ * the Larger Works (as defined below), to deal in both
+ *
+ * (a) the Software, and
+ *
+ * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+ * one is included with the Software each a "Larger Work" to which the Software
+ * is contributed by such licensors),
+ *
+ * without restriction, including without limitation the rights to copy, create
+ * derivative works of, display, perform, and distribute the Software and make,
+ * use, sell, offer for sale, import, export, have made, and have sold the
+ * Software and the Larger Work(s), and to sublicense the foregoing rights on
+ * either these or other terms.
+ *
+ * This license is subject to the following condition:
+ *
+ * The above copyright notice and either this complete permission notice or at a
+ * minimum a reference to the UPL must be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.oracle.labs.repl.util;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -11,6 +55,7 @@ public class ZipUtils {
 
     /**
      * Unzips given InputStream to given File
+     *
      * @param source zip stream
      * @param target target directory
      * @throws IOException extraction failed
@@ -40,9 +85,10 @@ public class ZipUtils {
 
     /**
      * Copies given InputStream to OutputStream
-     * @param source
-     * @param target
-     * @throws IOException
+     *
+     * @param source Source InputStream
+     * @param target Target InputStream
+     * @throws IOException If write failed
      */
     private static void copy(final InputStream source, final OutputStream target) throws IOException {
         final int bufferSize = 4 * 1024;
